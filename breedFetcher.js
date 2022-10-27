c989ad3c3e9083be3caa3a246f41edbf72849deb
Charlie <charlieryancurry@gmail.com>
2022-10-27 16:07:34 -0700

    moving to laptop

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
-const args = process.argv.splice(2);
 const request = require('request');
-const breedFetcher = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error !== null) {
-      console.log(error);
+      callback(error, undefined);
     } else if (error === null) {
       const data = JSON.parse(body);
       if (data[0] === undefined) {
-        console.log("error: Breed not found");
+        callback(null, null);
       } else {
-        console.log(data[0]);
+        callback(null, data[0]);
       }
     }
   });
 };
 
-breedFetcher(args[0]);
+module.exports = { fetchBreedDescription };
+
